63c5d334b992fda033b5a7354ca7e91d0043a8fb
Aryan <112301041@smail.iitpkd.ac.in>
2026-01-11 13:08:15 +0530

    solved ./makeAP.cpp

---
+++ CP31/cp_900/makeAP.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(( (2*b-c>0) && ((2*b - c)%a )== 0) || ( ((a+c)>0) && ((a+c)%(2*b) == 0)) || ( (2*b-a > 0) && (((2*b-a)%c) == 0)))cout<<"YES\n";
+        else cout<<"NO\n";
+
+    }
+}

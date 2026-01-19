28b2dd73fdb7033b4668061d07c52efceeb329c9
aryan <aryan0312g@outlook.com>
2026-01-19 10:15:33 +0530

    solved ./permutationSwap.cpp

---
+++ CP31/cp_900/permutationSwap.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> p(n);
+        for(int i=0;i<n;i++)cin>>p[i];
+        int res=0;
+        for(int i=0;i<n;i++){
+            res=__gcd(res,abs(p[i]-(i+1)));
+        }
+        cout<<res<<endl;
+    }
+}
